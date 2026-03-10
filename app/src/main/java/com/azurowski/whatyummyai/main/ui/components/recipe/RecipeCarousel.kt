33123bf7f4e9427675036a500ca0a7ad498a6e9c
aleksz <alekszurowski@gmail.com>
2026-03-10 19:33:16 +0100

    Recipe Screen in progress

---
+++ app/src/main/java/com/azurowski/whatyummyai/main/ui/components/recipe/RecipeCarousel.kt
@@ -0,0 +1,52 @@
+package com.azurowski.whatyummyai.main.ui.components.recipe
+
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.carousel.HorizontalMultiBrowseCarousel
+import androidx.compose.material3.carousel.rememberCarouselState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.azurowski.whatyummyai.R
+
+@Composable
+fun RecipeCarousel() {
+    data class CarouselItem(
+        val id: Int,
+        @DrawableRes val imageResId: Int,
+        val contentDescription: String
+    )
+
+    val items = remember {
+        listOf(
+            CarouselItem(0, R.drawable.pizza1, "pizza"),
+            CarouselItem(1, R.drawable.pizza2, "pizza")
+        )
+    }
+
+    HorizontalMultiBrowseCarousel(
+        state = rememberCarouselState { items.count() },
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight(),
+        preferredItemWidth = 250.dp,
+        itemSpacing = 8.dp,
+    ) { i ->
+        val item = items[i]
+        Image(
+            modifier = Modifier
+                .height(205.dp)
+                .maskClip(MaterialTheme.shapes.extraLarge),
+            painter = painterResource(id = item.imageResId),
+            contentDescription = item.contentDescription,
+            contentScale = ContentScale.Crop
+        )
+    }
+}
--- app/src/main/java/com/azurowski/whatyummyai/main/ui/screens/recipe/RecipeScreen.kt
+++ app/src/main/java/com/azurowski/whatyummyai/main/ui/screens/recipe/RecipeScreen.kt
@@ -11,7 +11,9 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.Button
@@ -25,12 +27,14 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.azurowski.whatyummyai.main.ui.components.recipe.RecipeCarousel
 import com.azurowski.whatyummyai.main.ui.theme.White50
 
 @Composable
@@ -94,8 +98,22 @@ fun RecipeScreen(navController: NavController, recipeId: String, recipeTitle: St
 
             Column(
                 modifier = Modifier
-                    .padding(vertical = 40.dp, horizontal = 30.dp)
+                    .padding(vertical = 30.dp, horizontal = 30.dp)
+                    .verticalScroll(rememberScrollState())
             ) {
+                RecipeCarousel()
+
+                Text(
+                    text = "Składniki",
+                    style = TextStyle(
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 24.sp,
+                        textAlign = TextAlign.Center
+                    ),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 32.dp)
+                )
 
             }
 
